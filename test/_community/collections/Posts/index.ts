2d1b42ef0a1813ea65883eec9bb107295c062860
Janisvb <jvb@vbinternet.de>
2024-01-15 11:40:02 +0100

    relationship search bug

--- test/_community/collections/Posts/index.ts
+++ test/_community/collections/Posts/index.ts
@@ -5,10 +5,19 @@ import { mediaSlug } from '../Media'
 export const postsSlug = 'posts'
 
 export const PostsCollection: CollectionConfig = {
+  admin: {
+    useAsTitle: 'text',
+  },
+  defaultSort: 'updatedAt',
   fields: [
     {
       name: 'text',
       type: 'text',
+    },
+    {
+      name: 'post',
+      type: 'relationship',
+      relationTo: 'posts',
     },
     {
       name: 'associatedMedia',
