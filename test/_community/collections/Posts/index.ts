e2d3aac07e642d8f6b6fc2d5cb9b603b435cc50d
Janisvb <jvb@vbinternet.de>
2024-01-22 11:09:02 +0100

    autosave

--- test/_community/collections/Posts/index.ts
+++ test/_community/collections/Posts/index.ts
@@ -1,10 +1,18 @@
 import type { CollectionConfig } from '../../../../packages/payload/src/collections/config/types'
-
 import { mediaSlug } from '../Media'
+import { APIError } from '../../../../packages/payload/src/errors'
 
 export const postsSlug = 'posts'
 
+const beforeChangeHook = async ({ data, req, operation, originalDoc }) => {
+  if (!data.text) throw new APIError('api error', 400, null, true) // public error
+}
+
 export const PostsCollection: CollectionConfig = {
+  versions: { drafts: { autosave: true } },
+  hooks: {
+    beforeChange: [beforeChangeHook],
+  },
   fields: [
     {
       name: 'text',
